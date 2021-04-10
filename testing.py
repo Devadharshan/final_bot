f0798e074c980d37caabfbe3e7a18dae973faf6e
Devadharshan <59635817+Devadharshan@users.noreply.github.com>
2021-04-11 00:47:13 +0530

    Initial commit files
    Working files needs json updated

---
+++ testing.py
@@ -0,0 +1,7 @@
+from torch import nn, optim
+import torch.nn.functional as F
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Assume that we are on a CUDA machine, then this should print a CUDA device:
+
+print(device)
